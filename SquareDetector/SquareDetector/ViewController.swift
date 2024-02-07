f9a0475efbc9446f2f4552ee57ac86d381426fe4
KimRoks <rudfhr3161@naver.com>
2024-02-07 17:12:58 +0900

    initial commti

---
+++ SquareDetector/SquareDetector/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SquareDetector
+//
+//  Created by 김경록 on 2/7/24.
+//
+
+import UIKit
+
+class DetectorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
